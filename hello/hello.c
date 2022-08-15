1d1115fbb49d4382ed51e84674bd76255fdf864e
asif sikdar <asifsikdar777@gmail.com>
2022-08-16 00:13:53 +0600

    16-08-22

--- hello/hello.c
+++ hello/hello.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 int main()
 {
-   printf("Hello, World!");
-   return 0;
+    int num1= 10,qube,result;
+    Sum(qube);
+    qube = num1*num1*num1+sum;
+
+    printf("value %d",qube);
+
+    return qube;
+}
+
+int Sum(qube)
+{
+    int a=10,b=15,sum;
+    sum = a+b;
+    //printf("value %d",sum);
+    return sum;
 }
