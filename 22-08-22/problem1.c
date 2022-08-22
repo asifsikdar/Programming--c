b77ed5fd6037e3fb72d9e8cdd4d95664e46404e7
asif sikdar <asifsikdar777@gmail.com>
2022-08-22 23:48:57 +0600

    5th day

---
+++ 22-08-22/problem1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    float x,y,count;
+    printf("Enter Teddy and eddy weight");
+    scanf("%f %f",&x,&y);
+
+    count =0;
+    while(x<=y)
+    {
+        x*=3;
+        y*=2;
+
+        count++;
+
+    }
+
+
+    printf("Result %.2f",count);
+}
