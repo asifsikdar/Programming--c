064503d4e6bc2808328fe27731aaf36346695ec6
asif sikdar <asifsikdar777@gmail.com>
2022-09-15 21:51:18 +0600

    p-d-1

---
+++ 04-09-22/test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int number,number1,result;
+    printf("Enter Number");
+    scanf("%d",&number);
+     printf("Enter Number2");
+    scanf("%d",&number1);
+    result = number + number1;
+    printf("%d",result);
+
+}
